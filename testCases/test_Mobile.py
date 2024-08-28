41104a2a193297200a26685e62375ac021b6c8f5
Prasanna <prasu2307@gmail.com>
2024-08-28 23:08:19 +0530

    Enhancements

--- testCases/test_Mobile.py
+++ testCases/test_Mobile.py
@@ -43,8 +43,6 @@ class Test_appium_mobile:
         options = UiAutomator2Options()
         options.platform_name = 'Android'
         options.udid = 'emulator-5554'
-        # options.app_package = 'com.google.android.youtube'
-        # options.app_activity = 'com.google.android.youtube.app.honeycomb.Shell$HomeActivity'
         # options.device_name = 'Pixel 8 Pro API 35'
         options.device_name = 'Medium Phone API 35'
         options.automation_name = 'UiAutomator2'
@@ -55,75 +53,44 @@ class Test_appium_mobile:
         # request.cls.driver = driver
         yield driver
         driver.quit()
-    @pytest.mark.mobile
-    @pytest.mark.flaky(reruns=2, reruns_delay=2)
-    def test_example(self,extra,appium_driver,env):
 
+    @pytest.mark.mobile
+    @pytest.mark.flaky(reruns=1, reruns_delay=2)
+    def test_example(self,extra,appium_driver,env):
 
         driver=appium_driver
         base = Base(driver, extra)
-        # Click on search icon
-        base.check_presence_of_AndroidElement('get_products_btn',env)
-        base.attach_snap("Application Home Page", UnivWaitFor=10)
-        # search_icon.click()
-        base.androidClick('get_products_btn',env)
-        base.check_presence_of_AndroidElement('product_name', env)
-        base.attach_snap("Product Page", UnivWaitFor=10)
+        with allure.step(f'Validating APK data'):
+            # Click on search icon
+            base.check_presence_of_AndroidElement('get_products_btn',env)
+            base.attach_snap("Application Home Page", UnivWaitFor=10)
+            # search_icon.click()
+            base.androidClick('get_products_btn',env)
+            base.check_presence_of_AndroidElement('product_name', env)
+            base.attach_snap("Product Page", UnivWaitFor=10)
 
-        strProductName = base.get_Androidtext('product_name',env)
-        strProductDesc = base.get_Androidtext('product_desc',env)
-        strProductPrice = base.get_Androidtext('product_price',env)
-        strProductPrice = str(strProductPrice).replace('₹', '')
-        strTeamName = ReadConfig.getEpsilonTeamName()
+            strProductName = base.get_Androidtext('product_name',env)
+            strProductDesc = base.get_Androidtext('product_desc',env)
+            strProductPrice = base.get_Androidtext('product_price',env)
+            strProductPrice = str(strProductPrice).replace('₹', '')
+            strTeamName = ReadConfig.getEpsilonTeamName()
 
-        # Creating a list with Product details
-        product_keylist = ['strProductName','strProductDesc','strProductPrice','strTeamName']
-        product_valuelist = [strProductName,strProductDesc,strProductPrice,strTeamName]
-        print('product_keylist\n',product_keylist)
-        print('product_valuelist\n',product_valuelist)
+            # Creating a list with Product details
+            product_keylist = ['strProductName','strProductDesc','strProductPrice','strTeamName']
+            product_valuelist = [strProductName,strProductDesc,strProductPrice,strTeamName]
+            print('product_keylist\n',product_keylist)
+            print('product_valuelist\n',product_valuelist)
 
-        # Dictionary to hold key and value pair from APK
-        dict_product_details= dict(zip(product_keylist,product_valuelist))
+            # Dictionary to hold key and value pair from APK
+            dict_product_details= dict(zip(product_keylist,product_valuelist))
 
-        print('dict_product_details\n',dict_product_details)
+            print('dict_product_details\n',dict_product_details)
 
-        # Create a list of dict
-        list_of_dict_product_details = [dict_product_details]
+            # Create a list of dict
+            list_of_dict_product_details = [dict_product_details]
 
-        print('list_of_dict_product_details\n',list_of_dict_product_details)
-
-        # # Dictionary to hold key and value pair from API
-        # api_list_of_dict_product_details=[{'strProductName': 'JBL Wireless Earbuds', 'strProductDesc': 'High-quality wireless earbuds with noise cancellation and 20 hours of battery life.', 'strProductPrice': '₹5999.0', 'strTeamName': 'Epsilon Team 2'}]
-        # api_dict_product_details = api_list_of_dict_product_details[0]
-        # print('api_list_of_dict_product_details\n', api_list_of_dict_product_details)
-        # print('api_dict_product_details\n', api_dict_product_details)
-        #
-        # # Check if they have the same values
-        # same_values_flag = sorted(dict_product_details.values()) == sorted(api_dict_product_details.values())
-        # print(same_values_flag)
-        # # assert same_values_flag, "Data is not Matched with API and APK"
-        # if same_values_flag:
-        #     print('Data is Matched with API and APK')
-        #     msg.showinfo('SUCCESS!!', 'Data is Matched with API and APK')
-        # else:
-        #     print('Data not Matched with API and APK')
-        #     msg.showerror('ERROR!!', 'Data not Matched with API and APK')
-        #
-        # # Open a file in write mode
-        # with open('Outputs/ActualOutputs/product_information_apk_result.txt', 'w') as file:
-        #     file.write(str(list_of_dict_product_details))
-
-        # # Open a file in write mode
-        # with open('Outputs/ActualOutputs/product_information_apk_result.txt', 'w', encoding='utf-8') as file:
-        #     file.write(str(list_of_dict_product_details))
+            print('list_of_dict_product_details\n',list_of_dict_product_details)
 
         with allure.step(f'Validating API using APPIUM data'):
             # Validating the API requests using data retrieved from APPIUM
             validate_api(list_of_dict_product_details)
-
-
-
-
-
-
-
--- testCases/test_ui.py
+++ testCases/test_ui.py
@@ -16,15 +16,7 @@ class Test_Sample:
     @pytest.mark.ui_test
     @pytest.mark.flaky(reruns=1, reruns_delay=2)
     def test_ui_site(self, extra, env, request, caseid):
-        # Removing the allure-report json files before the test runs
-        if os.path.exists(output_user_logpath + '\\Allure_reports\\'):
-            for root, dirs, files in os.walk(output_user_logpath + '\\Allure_reports\\'):
-                list_of_files = os.listdir(output_user_logpath + '\\Allure_reports\\')
-                for file in list_of_files:
-                    if file.endswith(".json") or file.endswith(".png") or file.endswith(".attach") or file.endswith(
-                            ".txt"):
-                        os.remove(os.path.join(root, file))
-                break
+
         baseURL = ReadConfig.getApplicationURL(env)
         # Creating object of Sample class
         sample = Sample(self.driver, extra)
